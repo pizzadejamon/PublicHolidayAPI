bfa1e6798a72755272e266e7861912b5e3165173
pizzadejamon <marius@priehl.de>
2017-03-08 15:21:37 +0100

    First Server Version
    Server runs, responds to Post Request with JSON object.
    Next: add "heartbeat" and more countries.
    also: add documentation

--- countries/de.js
+++ countries/de.js
@@ -14,118 +14,118 @@ var phodays = {
 		"holidays":[
 			{
 				"name": "Neujahr",
-				"region": "Alle Bundesländer",
+				"region": "DE: Alle Bundesländer",
 				"date": "",
 				"type": 0,
 				"day": "01-01"
 			},
 			{
 				"name": "Heilige Drei Könige",
-				"region": "Baden-Württemberg, Bayern, Sachsen-Anhalt",
+				"region": "DE: Baden-Württemberg, Bayern, Sachsen-Anhalt",
 				"date": "",
 				"type": 0,
 				"day": "01-06"
 			},
 			{
 				"name": "Karfreitag",
-				"region": "Alle Bundesländer",
+				"region": "DE: Alle Bundesländer",
 				"date": "",
 				"type": 1,
 				"offset": -2
 			},
 			{
 				"name": "Ostersonntag",
-				"region": "Brandenburg, Hessen",
+				"region": "DE: Brandenburg, Hessen",
 				"date": "",
 				"type": 1,
 				"offset": 0
 			},
 			{
 				"name": "Ostermontag",
-				"region": "Alle Bundesländer",
+				"region": "DE: Alle Bundesländer",
 				"date": "",
 				"type": 1,
 				"offset": 1
 			},
 			{
 				"name": "Erster Mai, Tag der Arbeit",
-				"region": "Alle Bundesländer",
+				"region": "DE: Alle Bundesländer",
 				"date": "",
 				"type": 0,
 				"day": "05-01"
 			},
 			{
 				"name": "Christi-Himmelfahrt",
-				"region": "Alle Bundesländer",
+				"region": "DE: Alle Bundesländer",
 				"date": "",
 				"type": 1,
 				"offset": 39
 			},
 			{
 				"name": "Pfingstsonntag",
-				"region": "Brandenburg, Hessen",
+				"region": "DE: Brandenburg, Hessen",
 				"date": "",
 				"type": 1,
 				"offset": 49
 			},
 			{
 				"name": "Pfingstmontag",
-				"region": "Alle Bundesländer",
+				"region": "DE: Alle Bundesländer",
 				"date": "",
 				"type": 1,
 				"offset": 50
 			},
 			{
 				"name": "Fronleichnam",
-				"region": "Baden-Württemberg, Bayern, Hessen, Nordrhein-Westfalen, Rheinland-Pfalz, Saarland, Thüringen",
+				"region": "DE: Baden-Württemberg, Bayern, Hessen, Nordrhein-Westfalen, Rheinland-Pfalz, Saarland, Thüringen",
 				"date": "",
 				"type": 1,
 				"offset": 60
 			},
 			{
 				"name": "Mariä Himmelfahrt",
-				"region": "Bayern, Saarland",
+				"region": "DE: Bayern, Saarland",
 				"date": "",
 				"type": 0,
 				"day": "08-15"
 			},
 			{
 				"name": "Tag der deutschen Einheit",
-				"region": "Alle Bundesländer",
+				"region": "DE: Alle Bundesländer",
 				"date": "",
 				"type": 0,
 				"day": "10-03"
 			},
 			{
 				"name": "Reformation",
-				"region": "Brandenburg, Mecklenburg-Vorpommern, Sachsen, Sachsen-Anhalt, Thüringen",
+				"region": "DE: Brandenburg, Mecklenburg-Vorpommern, Sachsen, Sachsen-Anhalt, Thüringen",
 				"date": "",
 				"type": 0,
 				"day": "10-31"
 			},
 			{
 				"name": "Allerheiligen",
-				"region": "Baden-Württemberg, Bayern, Nordrhein-Westfalen, Rheinland-Pfalz, Saarland",
+				"region": "DE: Baden-Württemberg, Bayern, Nordrhein-Westfalen, Rheinland-Pfalz, Saarland",
 				"date": "",
 				"type": 0,
 				"day": "11-01"
 			},
 			{
 				"name": "Buß- und Bettag",
-				"region": "Sachsen",
+				"region": "DE: Sachsen",
 				"date": "",
 				"type": 5
 			},
 			{
 				"name": "Erster Weihnachtsfeiertag",
-				"region": "Alle Bundesländer",
+				"region": "DE: Alle Bundesländer",
 				"date": "",
 				"type": 0,
 				"day": "12-25"
 			},
 			{
 				"name": "Zweiter Weihnachtsfeiertag",
-				"region": "Alle Bundesländer",
+				"region": "DE: Alle Bundesländer",
 				"date": "",
 				"type": 0,
 				"day": "12-26"
@@ -154,10 +154,6 @@ function processForYear(year){
 	}
 	console.log(JSON.stringify(phodays));
 }
-
-processForYear(2017);
-
-
 
 
 
@@ -202,7 +198,7 @@ function processBettag(obj, year){
 //used in mains.js when requesting calender for country
 module.exports = {
 		getHolidays: function (year){
-			processForYear(2017);
+			processForYear(year);
 			return phodays;
 		}
 }
